5055b8ccb167ff7761d3357c4e0a9aa1c56a61e2
TomasNiessner <tomasniessner@outlook.com>
2019-04-04 18:42:06 -0300

    Primer programa

---
+++ Ejercicio_complementario.py
@@ -0,0 +1,34 @@
+number_to_guess = 14
+
+user_number = int(input("Adivina un numero: "))
+
+if number_to_guess == user_number:
+    print("Has ganado")
+
+else:
+    user_number = int(input("Adivina otra vez: "))
+    if number_to_guess == user_number:
+        print("has ganado")
+    else:
+        user_number = int(input("Adivina otra vez: "))
+        if number_to_guess == user_number:
+            print("has ganado")
+        else:
+            user_number = int(input("Adivina otra vez: "))
+            if number_to_guess == user_number:
+                print("has ganado")
+            else:
+                user_number = int(input("Adivina otra vez: "))
+                if number_to_guess == user_number:
+                    print("has ganado")
+                else:
+                    print("Has perdido")
+
+
+
+
+
+
+
+
+
